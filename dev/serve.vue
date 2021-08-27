3ad227df22679570909dc551c9c5a310c9af4c13
thetarnav <gthetarnav@gmail.com>
2021-08-27 23:14:40 +0200

    base wave setup

--- dev/serve.vue
+++ dev/serve.vue
@@ -1,17 +1,39 @@
-<script lang="ts">
-import { defineComponent } from 'vue';
-import WaveSkeleton from '@/wave-skeleton.vue';
-
-export default defineComponent({
-  name: 'ServeDev',
-  components: {
-    WaveSkeleton
-  }
-});
+<script setup lang="ts">
+import WaveSkeleton from '@/wave-skeleton.vue'
 </script>
 
 <template>
-  <div id="app">
-    <wave-skeleton />
-  </div>
+	<div class="layout">
+		<WaveSkeleton />
+		<WaveSkeleton />
+	</div>
 </template>
+
+<style lang="scss">
+html,
+body,
+#app {
+	height: 100vh;
+	margin: 0;
+	width: 100vw;
+}
+
+#app {
+	align-items: center;
+	justify-content: center;
+	display: flex;
+	flex-direction: column;
+	background: #f2f2f2;
+}
+
+.layout {
+	display: flex;
+	flex-direction: column;
+	align-items: flex-start;
+}
+
+.wave-skeleton {
+	height: 60px;
+	width: 200px;
+}
+</style>
--- src/wave-skeleton.vue
+++ src/wave-skeleton.vue
@@ -1,88 +1,104 @@
 <script lang="ts">
-import { defineComponent } from 'vue';
+import { defineComponent, h, onMounted, ref, watchEffect } from 'vue'
+import { useElementSize, templateRef, useElementBounding } from '@vueuse/core'
 
-interface SampleData {
-  counter: number;
-  initCounter: number;
-  message: {
-    action: string | null;
-    amount: number | null;
-  };
+function buildWave(path: Element, segments: number) {
+	const h = 60
+	const m = 0.512286623256592433
+	const a = h / 4
+	const y = h / 2
+
+	const addSegment = (left: number, multiply = 1): Array<string | number> => {
+		const segment = ['s', -(1 - a) * m, a * multiply, a, a * multiply]
+		left > 0 && segment.push(...addSegment(left - 1, multiply * -1))
+		return segment
+	}
+
+	const pathData = [
+		'M 0',
+		y + a / 2,
+		'c',
+		a * m,
+		0,
+		-(1 - a) * m,
+		-a,
+		a,
+		-a,
+		...addSegment(segments),
+	].join(' ')
+
+	path.setAttribute('d', pathData)
 }
+/*
+buildWave(paths[0], 60, 20);
+buildWave(paths[1], 60, 20);
+buildWave(paths[2], 60, 20);
+buildWave(paths[3], 60, 14);
+*/
 
-export default /*#__PURE__*/defineComponent({
-  name: 'WaveSkeleton', // vue component name
-  data(): SampleData {
-    return {
-      counter: 5,
-      initCounter: 5,
-      message: {
-        action: null,
-        amount: null,
-      },
-    };
-  },
-  computed: {
-    changedBy() {
-      const { message } = this as SampleData;
-      if (!message.action) return 'initialized';
-      return `${message.action} ${message.amount || ''}`.trim();
-    },
-  },
-  methods: {
-    increment(arg: Event | number): void {
-      const amount = (typeof arg !== 'number') ? 1 : arg;
-      this.counter += amount;
-      this.message.action = 'incremented by';
-      this.message.amount = amount;
-    },
-    decrement(arg: Event | number): void {
-      const amount = (typeof arg !== 'number') ? 1 : arg;
-      this.counter -= amount;
-      this.message.action = 'decremented by';
-      this.message.amount = amount;
-    },
-    reset(): void {
-      this.counter = this.initCounter;
-      this.message.action = 'reset';
-      this.message.amount = null;
-    },
-  },
-});
+export default defineComponent({
+	name: 'WaveSkeleton',
+	setup() {
+		const path = templateRef('path')
+		const el = templateRef('el')
+		const { width, height } = useElementSize(el)
+
+		watchEffect(() => {
+			width.value
+			height.value
+			if (width.value > 0 && height.value > 0 && path.value)
+				buildWave(path.value, 20)
+		})
+	},
+})
 </script>
 
 <template>
-  <div class="wave-skeleton">
-    <p>The counter was {{ changedBy }} to <b>{{ counter }}</b>.</p>
-    <button @click="increment">
-      Click +1
-    </button>
-    <button @click="decrement">
-      Click -1
-    </button>
-    <button @click="increment(5)">
-      Click +5
-    </button>
-    <button @click="decrement(5)">
-      Click -5
-    </button>
-    <button @click="reset">
-      Reset
-    </button>
-  </div>
+	<div ref="el" class="wave-skeleton">
+		<svg viewBox="5 15 212 30">
+			<path ref="path"></path>
+		</svg>
+	</div>
 </template>
 
-<style scoped>
-  .wave-skeleton {
-    display: block;
-    width: 400px;
-    margin: 25px auto;
-    border: 1px solid #ccc;
-    background: #eaeaea;
-    text-align: center;
-    padding: 25px;
-  }
-  .wave-skeleton p {
-    margin: 0 0 1em;
-  }
+<style lang="scss">
+.wave-skeleton {
+	height: clamp(20px, 6vmin, 40px);
+	margin: 0;
+	overflow: hidden;
+	animation: wave-skeleton-fade 800ms infinite alternate;
+
+	path {
+		fill: none;
+		stroke: #828282;
+		stroke-width: 5.2px;
+		stroke-linecap: round;
+		stroke-dasharray: 0 16 300 16;
+		animation: wave-skeleton-wave 2400ms linear infinite;
+	}
+
+	// svg:last-of-type .wave {
+	// 	stroke-dasharray: 0 16 170 16;
+	// }
+}
+
+@keyframes wave-skeleton-wave {
+	0% {
+		stroke-dashoffset: 0;
+		transform: translate3d(0, 0, 0);
+	}
+	100% {
+		stroke-dashoffset: -133;
+		transform: translate3d(-90px, 0, 0);
+	}
+}
+
+@keyframes wave-skeleton-fade {
+	0% {
+		opacity: 0.5;
+	}
+	100% {
+		opacity: 1;
+	}
+}
 </style>
