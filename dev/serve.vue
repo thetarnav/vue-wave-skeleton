299a7b09efc3f6d713a263817f0c1a92b430e7c7
thetarnav <gthetarnav@gmail.com>
2021-08-27 22:37:43 +0200

    `vue-sfc-rollup` template init

---
+++ dev/serve.vue
@@ -0,0 +1,17 @@
+<script lang="ts">
+import { defineComponent } from 'vue';
+import WaveSkeleton from '@/wave-skeleton.vue';
+
+export default defineComponent({
+  name: 'ServeDev',
+  components: {
+    WaveSkeleton
+  }
+});
+</script>
+
+<template>
+  <div id="app">
+    <wave-skeleton />
+  </div>
+</template>
